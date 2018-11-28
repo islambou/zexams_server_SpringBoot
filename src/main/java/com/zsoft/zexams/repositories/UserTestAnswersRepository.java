b86de4a6335e57a0e1a5873eabfc86a5fc55db95
Islam <islambou@gmail.com>
2018-11-28 01:41:04 +0100

    updating user test answers repository

--- src/main/java/com/zsoft/zexams/repositories/UserTestAnswersRepository.java
+++ src/main/java/com/zsoft/zexams/repositories/UserTestAnswersRepository.java
@@ -4,9 +4,13 @@ import com.zsoft.zexams.modules.UserTestAnswers;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 
 @Repository
 public interface UserTestAnswersRepository extends CrudRepository<UserTestAnswers, String> {
+
+    ArrayList<UserTestAnswers> findByUser(String user);
 
 
 }
